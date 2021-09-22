073fafcbf9411e8b8e1a84a69f5dded75877d8da
Julien Kherroubi <julien.kherroubi@epita.fr>
2021-09-22 19:33:48 +0200

    feature(physic): add the right turn

--- game/physic.js
+++ game/physic.js
@@ -2,12 +2,13 @@ function update()
 {
     var delta = clock.getDelta(); // seconds.
     var moveDistance = 50 * delta; // 200 pixels per second
-    var rotateAngle = Math.PI / 2 * delta * 2;   // pi/2 radians (90 degrees) per second
+    var rotateAngleL = Math.PI / 2 * delta * 2;   // pi/2 radians (90 degrees) per second
+    var rotateAngleR = - Math.PI / 2 * delta * 2; // - pi/2 radians (90 degrees) per second
 
     if (keyboard.pressed("left"))
-        player1.turnLeft(rotateAngle);
+        player1.turnLeft(rotateAngleL);
     if (keyboard.pressed("right"))
-        player1.turnRight(rotateAngle);
+        player1.turnRight(rotateAngleR);
     if (keyboard.pressed("up"))
         player1.accelerate(moveDistance);
     if (keyboard.pressed("down"))
